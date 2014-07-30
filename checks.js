8ab06e9c1b45c1e6fa15d934c68e2c974242bffd
Jim Kang <jimkang@gmail.com>
2014-07-30 00:53:26 -0400

    Added checks.branchOnError.

--- checks.js
+++ checks.js
@@ -7,6 +7,18 @@ function checkError(error, done) {
   return noError;
 }
 
+// Intended to be used with currying.
+function branchOnError(onFail, onSuccess, error) {
+  if (error) {
+    onFail(error);
+  }
+  else {
+    var args = Array.prototype.slice.call(arguments, 3);
+    onSuccess.apply(onSuccess, args);
+  }
+}
+
 module.exports = {
-  checkError: checkError
+  checkError: checkError,
+  branchOnError: branchOnError
 };
