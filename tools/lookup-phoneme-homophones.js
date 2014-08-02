3ada9fade9d312b5953c48e9d791dcc7c31c9892
Jim Kang <jimkang@gmail.com>
2014-08-02 19:19:29 -0400

    Cleaned out comments.

--- tools/lookup-phoneme-homophones.js
+++ tools/lookup-phoneme-homophones.js
@@ -5,7 +5,6 @@ var homophonizer = homophonizerFactory({
 var probable = require('probable');
 
 var word = process.argv[2];
-// var varyAtPositionsString = process.argv[3]; // e.g. "[1, 3]"
 var imperfect = process.argv[3];
 
 if (word.length > 0) {
@@ -18,7 +17,7 @@ if (word.length > 0) {
       else {
         var phonemes = phonemeString.split(' ');
         varyAtPositions = phonemes.map(function getPosition(ph, i) { return i; });
-        // console.log('varyAtPositions', varyAtPositions);
+
         homophonizer.getImperfectHomophones({
           word: word,
           varyPhonemesAtPositions: varyAtPositions
